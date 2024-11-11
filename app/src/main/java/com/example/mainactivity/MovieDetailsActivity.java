cf37064c63ef56c20021c9b7233c4bc954fedb59
isulimanm <isulimanm@gmail.com>
2024-11-12 02:12:24 +0300

    2:00

--- app/src/main/java/com/example/mainactivity/MovieDetailsActivity.java
+++ app/src/main/java/com/example/mainactivity/MovieDetailsActivity.java
@@ -1,6 +1,9 @@
 package com.example.mainactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +19,31 @@ public class MovieDetailsActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_movie_details);
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_watchlist) {
+            // Navigate to WatchListActivity
+            Intent intent = new Intent(this, WatchListActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_favorites) {
+            // Navigate to FavoritesActivity
+            Intent intent = new Intent(this, FavoritesActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
